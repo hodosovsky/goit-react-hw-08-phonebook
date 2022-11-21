9f8411d1d830012a53f31c981e3cf77599cf40ee
hodosovsky <107750587+hodosovsky@users.noreply.github.com>
2022-11-21 15:39:49 +0200

    moved function up

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,6 +12,33 @@ class App extends Component {
     filter: '',
   };
 
+  componentDidMount() {
+    const defaultContacts = [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ];
+    const contactsFromLocaleStorage = JSON.parse(
+      localStorage.getItem('contacts')
+    );
+
+    if (contactsFromLocaleStorage) {
+      this.setState({ contacts: contactsFromLocaleStorage });
+    } else {
+      this.setState({ contacts: defaultContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      console.log('Оновився');
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
+  componentWillUnmount() {}
+
   getVisibleContacts = () => {
     const normalaziedFilter = this.state.filter.toLowerCase();
     return this.state.contacts.filter(contact =>
@@ -49,33 +76,6 @@ class App extends Component {
       contacts: deletedList,
     }));
   };
-
-  componentDidMount() {
-    const defaultContacts = [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ];
-    const contactsFromLocaleStorage = JSON.parse(
-      localStorage.getItem('contacts')
-    );
-
-    if (contactsFromLocaleStorage) {
-      this.setState({ contacts: contactsFromLocaleStorage });
-    } else {
-      this.setState({ contacts: defaultContacts });
-    }
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.contacts !== prevState.contacts) {
-      console.log('Оновився');
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
-
-  componentWillUnmount() {}
 
   render() {
     const visibleContacts = this.getVisibleContacts();
