9b35d715e62c5b3b81374fa8b24c153d3efc658d
hodosovsky <107750587+hodosovsky@users.noreply.github.com>
2022-11-21 14:51:36 +0200

    add contacts to localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,12 +8,7 @@ import Contacts from './Contacts/Contacts';
 import Filter from './Filter/Filter.jsx';
 class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
 
@@ -54,6 +49,33 @@ class App extends Component {
       contacts: deletedList,
     }));
   };
+
+  componentDidMount() {
+    const defaultContacts = [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ];
+    const contactsFromLocaleStorage = JSON.parse(
+      localStorage.getItem('contacts')
+    );
+
+    if (contactsFromLocaleStorage) {
+      this.setState({ contacts: contactsFromLocaleStorage });
+    } else {
+      this.setState({ contacts: defaultContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      console.log('Оновився');
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
+  componentWillUnmount() {}
 
   render() {
     const visibleContacts = this.getVisibleContacts();
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 
-import { MyForm } from './Form,styled';
+import { MyForm } from './Form.styled';
 const { Component } = require('react');
 
 class Form extends Component {
@@ -8,7 +8,7 @@ class Form extends Component {
     name: '',
     number: '',
   };
-  addContact = event => {
+  handleAddContact = event => {
     const { name, value } = event.currentTarget;
     this.setState({ [name]: value });
   };
@@ -33,7 +33,7 @@ class Form extends Component {
             type="text"
             name="name"
             value={this.state.name}
-            onChange={this.addContact}
+            onChange={this.handleAddContact}
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
@@ -46,7 +46,7 @@ class Form extends Component {
             type="tel"
             name="number"
             value={this.state.number}
-            onChange={this.addContact}
+            onChange={this.handleAddContact}
             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             required
