110c3871beb50687dd1199d0b9d13785c312b934
hodosovsky <107750587+hodosovsky@users.noreply.github.com>
2022-12-09 11:17:15 +0200

    add dev tools

--- src/redux/contacts/action.users.js
+++ src/redux/contacts/action.users.js
@@ -1,5 +1,5 @@
 import { createAction } from '@reduxjs/toolkit';
 
-export const getUsersPendingAction = createAction('users/getUsersPending');
-export const getUsersFulfilledAction = createAction('users/getUsersFulfilled');
-export const getUsersRejectedAction = createAction('users/getUsersRejected');
+// export const getUsersPendingAction = createAction('users/getUsersPending');
+// export const getUsersFulfilledAction = createAction('users/getUsersFulfilled');
+// export const getUsersRejectedAction = createAction('users/getUsersRejected');
